80e151b316be2425878f5cc9884a0789f0e62ba3
AlessandroVola <volaalessandro@gmail.com>
2022-09-26 22:48:46 +0200

    base

---
+++ dischi/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HeaderComponent/>
+    <MainComponent/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent
+  }
+  }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+}
+</style>
---
+++ dischi/src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <p>Spotify Logo</p>
+    </header>
+    
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 10vh;
+    background-color: grey;
+}
+
+</style>
